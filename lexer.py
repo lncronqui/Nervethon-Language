d1e1c1eede38310a85125d43cd58bffc0e367169
Lance <ronquillolance@gmail.com>
2021-12-09 23:39:43 +0800

    push

--- lexer.py
+++ lexer.py
@@ -13,33 +13,37 @@ def run(lexeme):
     keywords= {'Link.Start', 'Link.End', 'Generate', 'Sys', 'Sys.Call', 'Discharge', 'Absorb', 'If', 'Elif', 'Else', 'Switch', 'Execute', 'Default', 'For', 'While', 'Exit', 'Continue', 'Avoid', 'Fixed', 'Struct', 'Void', 'Return'}
     datatype = {'Integer','Boolean','String','Decimal'} 
     token_specification = [
-        ('ARITHMETIC', r'[+]|[\-]|[\*]|[/]|[%]|[\*][\*]|[/][/]'),
-        ('RELATIONAL', r'[<]|[>]|[=][=]|[!][=]|[>][=]|[<][=]'),
-        ('ASSIGNMENT', r'[=]|[\+][=]|[\-][=]|[][=]|[/][=]|[/][/][=]|[%][=]|[\*][\*][=]'),
+        ('ARITHMETIC', r'([+]|[\-]|[\*]|[/]|[%]|[\*][\*]|[/][/])'),
+        ('RELATIONAL', r'([<]|[>]|[=][=]|[!][=]|[>][=]|[<][=])'),
+        ('ASSIGNMENT', r'([=]|[\+][=]|[\-][=]|[][=]|[/][=]|[/][/][=]|[%][=]|[\*][\*][=])'),
         ('LOGICAL', r'[A][n][d]|[O][r]|[N][o][t]'),
         ('SYMBOLS', r'[(] | [)] | [{] | [}] | [[] | []] | ["] | ["]'),
         ('COMMENT', r'[/][\*]|[\*][/]'),
         ('ESCAPESEQ', r'[\n] | [\t] | [\"] | [\'] | [\\]'),
         ('ID', r'[a-z]\w{0,19}'),
-        ('LIT_INTPOSI', r'[1-9][\d]{0,8}'),
-        ('LIT_INTNEGA', r'-[1-9][\d]{0,8}'),
-        ('LIT_DECPOSI', r'[0-9]{1,9}.[0-9]{1,6}'),
-        ('LIT_DECNEGA', r'-[0-9]{1,9}.[0-9]{1,6}'),
+        ('RESERVED_WORD', r'[A-Z][\w\.]*'),
+        ('NUMBER', r'\d+(\.\d{1,6})?'),
+        #('LIT_INTPOS', r'[\d]{1,9}'),
+        #('LIT_INTNEG', r'^\-[1-9][\d]{0,8}$'),
+        #('LIT_DECPOS', r'^[\d]{1,9}\.[0-9]{1,6}$'),
+        #('LIT_DECNEG', r'^\-[\d]{1,9}\.[0-9]{1,6}$'),
+        #('LIT_POS', r'\d{1,9}+(\.\d){1,6}'),
+        #('LIT_NEG', r'\-\d{1,9}+(\.\d){1,6}'),
         ('LIT_STRING', r'\"[a-zA-Z]\"'),
         ('LIT_BOOL', r'[T][r][u][e]|[F][a][l][s][e]'),
-        ('SPACE', r'[ +]'),
-        ('SKIP', r'[\t+]'),
+        ('SPACE', r'[ ]+'),
+        ('SKIP', r'[\t]+'),
         ('NEWLINE', r'\n'),
-        #('MISMATCH', r'.'),
+        ('MISMATCH', r'.'),
     ]
 
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
     line_num = 1
     line_start = 0
-    for x in re.finditer(tok_regex, lexeme):
-        kind = x.lastgroup
-        value = x.group()
-        column = x.start() - line_start
+    for mo in re.finditer(tok_regex, lexeme):
+        kind = mo.lastgroup
+        value = mo.group()
+        column = mo.start() - line_start
         #if --> arithmetic, relational, assignment, logical, symbols, comments, id, literals
         if kind == 'ARITHMETIC':
             kind = value
@@ -55,24 +59,28 @@ def run(lexeme):
             kind = value
         elif kind == 'ID':
             kind = value
-        elif value in keywords:
+        elif kind == 'RESERVED_WORD' and value in keywords:
             kind = value
-        elif kind == 'LIT_INTPOSI':
-            value = int(value)
-        elif kind == 'LIT_INTNEGA':
-            value = int(value)
-        elif kind == 'LIT_DECPOSI':
-            value = float(value)
-        elif kind == 'LIT_DECNEGA':
-            value = float(value)
+        elif kind == 'NUMBER':
+            value = float(value) if '.' in value else int(value)
+        #elif kind == 'LIT_INTPOS':
+        #    value = int(value)
+        #elif kind == 'LIT_INTNEG':
+        #    value = int(value)
+        #elif kind == 'LIT_DECPOS':
+        #    value = float(value) if '.' in value else int(value)
+        #elif kind == 'LIT_DECNEG':
+        #    value = float(value) if '.' in value else int(value)
+        #elif kind == 'LIT_POSI':
+        #    value = float(value) if '.' in value else int(value)
+        #elif kind == 'LIT_NEGA':
+        #    value = float(value) if '.' in value else int(value)
         elif kind == 'LIT_STRING':
-            kind = value
-        elif kind == 'LITERALS':
             kind = value
         elif kind == 'LIT_BOOL':
             kind = value
         elif kind == 'NEWLINE':
-            line_start = x.end()
+            line_start = mo.end()
             line_num += 1
             continue
         elif kind == 'SKIP':
@@ -80,3 +88,9 @@ def run(lexeme):
         elif kind == 'MISMATCH':
             raise RuntimeError(f'{value!r} unexpected on line {line_num}')
         yield Token(kind, value, line_num, column)
+        
+with open('user_input.txt', 'r') as file:
+    user_input = file.read()
+
+for result in run(user_input):
+    print(result)
