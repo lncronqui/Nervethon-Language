1434ed6ad6096d0165ec8e95cca8ea5e6464aefe
Ma. Pauline Yvana Amores <75066763+mapaulineyvanaamores@users.noreply.github.com>
2021-12-09 16:07:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -46,29 +46,29 @@ unary_plus		    = "^[+]$"
 unary_minus         = "^[-]$"
 
 #ARITHMETIC OPERATORS#
-arithop_mul         = "*"
-arithop_div         = "/"
-arithop_per         = "%"
-arithop_mod         = "**"
-arithop_exp         = "//"
+arithop_mul         = "^[*]$"
+arithop_div         = "^[/]$"
+arithop_per         = "^[%]$"
+arithop_mod         = "^[*][*]$"
+arithop_exp         = "^[/][/]$"
 
 #RELATIONAL OPERATORS#
-relateop_gthan          = "<"
-relateop_lthan          = ">"
-relateop_eqto           = "=="
-relateop_noteqto        = "!="
-relateop_gthanoreqto    = ">="
-relateop_lthanoreqto    = "<="
+relateop_gthan          = "^[<]$"
+relateop_lthan          = "^[>]$"
+relateop_eqto           = "^[=][=]$"
+relateop_noteqto        = "^[!][=]$"
+relateop_gthanoreqto    = "^[>][=]$"
+relateop_lthanoreqto    = "^[<][=]$"
 
 #ASSIGNMENT OPERATORS#
-assignop_simple     = "[=]"
-assignop_addAND     = "[+][=]"
-assignop_subAND     = "[-][=]"
-assignop_multAND    = "[*=]"
-assignop_divAND     = "[/=]"
-assignop_floorAND   = "[//=]"
-assignop_modAND     = "[%=]"
-assignop_exAND      = "[**=]"
+assignop_simple     = "^[=]$"
+assignop_addAND     = "^[+][=]$"
+assignop_subAND     = "^[-][=]$"
+assignop_multAND    = "^[*][=]$"
+assignop_divAND     = "^[/][=]$"
+assignop_floorAND   = "^[/][/][=]$"
+assignop_modAND     = "^[%][=]$"
+assignop_exAND      = "^[*][*][=]$"
 
 #LOGICAL OPERATORS#
 logicalop_and   = "(^And)"
@@ -76,43 +76,42 @@ logicalop_or    = "(^Or)"
 logicalop_not   = "(^Not)"
 
 #OPEN SYMBOLS#
-open_parenthesis    = "("
-open_curly          = "{"
-open_bracket        = "["
-open_quotation      = '"'
+open_parenthesis    = "^[(]$"
+open_curly          = "^[{]$"
+open_bracket        = "^[[]$"
+open_quotation      = "^\"$"
 
 #CLOSE SYMBOLS#
-close_parenthesis    = ")"
-close_curly          = "}"
-close_bracket        = "]"
-close_quotation      = '"'
+close_parenthesis    = "^[)]$"
+close_curly          = "^[}]$"
+close_bracket        = "^[]]$"
+close_quotation      = "^\"$"
 
 #COMMENT SYMBOLS#
-opening_comment     = '/*'
-closing_comment     = '*/'
+comment_full     = "^[/][*](:word:)[*][/]$"
 
 #ESCAPE SEQUENCES SYMBOLS#
-newline         = "\n"
-tab             = '\t'
-double_quote    = '\"'
-single_quote    = '\''
-backslash       = '\\'
+newline         = "^[\][n]$"
+tab             = "^[\][t]$"
+double_quote    = "^[\"]$"
+single_quote    = "^[\][']$"
+backslash       = "^[\][\]$"
 
 #OTHER SYMBOLS#
-color        = ":"
-period       = "."
-semicolon    = ";"
-comma        = ","
+colon        = "^[:]$"
+period       = "^[.]$"
+semicolon    = "^[;]$"
+comma        = "^[,]$"
 
 #IDENTIFIERS#
-identifier   = "^[a-z]([a-z\_A-Z0-9]{0,19})$"
+identifier   = "^[a-z][a-zA-Z0-9]{0,19}"
 
 #LITERALS#
-lit_intposi     = "^[1-9]{1,9}$"
+lit_intposi     = "^[1-9][\d]{0,8}$"
 lit_intnega     = "^\-([1-9]{1,9})$"   
 lit_decposi     = "^[0-9]{1,9}\.[0-9]{1,6}"
-lit_decnega     = "^\-([0-9]{1,9})\.[0-9]{1,6}"
-lit_string      = ""
+lit_decnega     = "^\-[0-9]{1,9}\.[0-9]{1,6}"
+lit_string      = "^\"[a-zA-Z]\"&"
 lit_bool        = "(True|False)"
 
 def keyword(input):
@@ -133,9 +132,27 @@ def datatype(input):
             continue
     return False
 
+def unary(input):
+    unary_op = [unary_minus, unary_plus]
+    for x in unary_op:
+        if(re.search(x, input)):
+            return True
+        else:
+            continue
+    return False
+
 def arith(input):
-    arith_op = [unary_minus, unary_plus]
+    arith_op = [arithop_mul, arithop_div, arithop_per, arithop_mod, arithop_exp]
     for x in arith_op:
+        if(re.search(x, input)):
+            return True
+        else:
+            continue
+    return False
+
+def rel(input):
+    relational_op = [relateop_gthan, relateop_lthan, relateop_eqto, relateop_noteqto, relateop_gthanoreqto, relateop_lthanoreqto]
+    for x in relational_op:
         if(re.search(x, input)):
             return True
         else:
@@ -150,6 +167,25 @@ def assign(input):
         else:
             continue
     return False
+
+def logical(input):
+    logic_op = [logicalop_and, logicalop_or, logicalop_not]
+    for x in logic_op:
+        if(re.search(x, input)):
+            return True
+        else:
+            continue
+    return False
+
+def symbols(input):
+    symbol_op = [open_parenthesis, open_curly, open_bracket, open_quotation, close_parenthesis, close_curly, close_bracket, close_quotation, newline, tab, backslash, colon, period, semicolon, comma]
+    for x in symbol_op:
+        if(re.search(x, input)):
+            return True
+        else:
+            continue
+    return False
+
 #######################################
 # REGEX
 #######################################
@@ -172,14 +208,29 @@ while(count < len(lexeme)):
     elif(datatype(current_char)==True):
         print(current_char + "\t-\tData Type")
     
+    #OPERATORS#
+    elif(unary(current_char)==True):
+        print(current_char + "\t-\tUnary Operator")
     elif(arith(current_char)==True):
         print(current_char + "\t-\tArithmetic Operator")
+    elif(rel(current_char)==True):
+        print(current_char + "\t-\tRelational Operator")
     elif(assign(current_char)==True):
         print(current_char + "\t-\tAssignment Operator")
+    elif(logical(current_char)==True):
+        print(current_char + "\t-\tLogical Operator")
+
+    #SYMBOLS#
+    elif(symbols(current_char)==True):
+        print(current_char + "\t-\tSymbol")
     elif(re.search("N_L", current_char)):
         print("\\n" + "\t-\tNew Line")
     elif(re.search(identifier, current_char)):
         print(current_char + "\t-\tid")
+    elif(re.search(comment_full, current_char)):
+        print(current_char + "\t-\tComments")
+    
+    #INTEGERS#
     elif(re.search(lit_intposi, current_char)):
         print(current_char + "\t-\tPositive Integer Literal")
     elif(re.search(lit_intnega, current_char)):
@@ -188,11 +239,10 @@ while(count < len(lexeme)):
         print(current_char + "\t-\tPositive Decimal Literal")
     elif(re.search(lit_decnega, current_char)):
         print(current_char + "\t-\tNegative Decimal Literal")
-    #elif(re.search(lit_string, current_char)):
-    #    print(current_char + "\t-\tString Literal")
+    elif(re.search(lit_string, current_char)):
+        print(current_char + "\t-\tString Literal")
     elif(re.search(lit_bool, current_char)):
         print(current_char + "\t-\tBoolean Literal")
     else:
         print(current_char + "\t-\tInvalid Input")
         
-          
