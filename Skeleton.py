0cb155ab69c3e52ebd204043f32befb83dc4b7c8
charisse21 <72638158+charisse21@users.noreply.github.com>
2021-12-10 13:03:06 +0800

    Working Scrollbars

--- Skeleton.py
+++ Skeleton.py
@@ -26,7 +26,6 @@ canvasLogo=Canvas(frame_top, width=270, height=55, borderwidth=0, highlightbackg
 canvasLogo.create_image(140,29, image=photo_imageNervethon)
 canvasLogo.place(x=20,y=5)
 
-
 #Frame 1 Top left size
 frame1=Frame(root, width=633, height=400, highlightbackground='#ffffff', highlightthickness=1)
 frame1.grid(row=2, column=0,padx=25, pady=25)
@@ -44,7 +43,7 @@ scrollbar.pack(side=RIGHT, fill=Y)
 text_area.pack(side="left")
 
 
-#TRIAL INPUT AND OUTPUT
+#INPUT AND OUTPUT
 def Take_input():
     Output.configure(state='normal')
     Errors.configure(state='normal')
@@ -72,29 +71,27 @@ def Take_input():
                 Errors.insert(END, '\n')
                 Errors.configure(state='disabled')
 
-        
-
-        
 #Frame 2 - Errors
 frame2=Frame(root, width=633, height=100, highlightbackground='#ffffff', bg='#121212', highlightthickness=1)
 frame2.grid(row=3, column=0, padx=25, pady=25, ipadx=25, ipady=25)
 
+#ERRORS TEXT AREA AND SCROLLBAR
 scrollbar2=ttk.Scrollbar(frame2, orient='vertical')
-scrollbar2.config(command=text_area.yview)
-scrollbar2.pack(side=RIGHT, fill=Y)
-text_area.pack(side="left")
-
 Errors = Text(frame2, width = 68, height = 5, font = ("Times New Roman",13), bg = "#121212", fg="White", highlightthickness=0, borderwidth=0, state='disabled',  yscrollcommand=scrollbar2.set)
-
+scrollbar2.config(command=Errors.yview)
+scrollbar2.pack(side=RIGHT, fill=Y)
+Errors.pack(side="left")
 
 #Frame 3 - Output
 frame3=Frame(root, width=520, height=660, highlightbackground='#ffffff', background='#121212', highlightthickness=1)
 frame3.grid(row=1, column=1, rowspan = 6, columnspan = 2, padx=25, pady=(65,25), ipadx=25, ipady=25)
 
+#OUTPUT TEXT AREA AND SCROLLBAR
 scrollbar3=ttk.Scrollbar(frame3, orient='vertical')
-scrollbar3.config(command=text_area.yview)
+Output = Text(frame3, width = 50, height =28, font = ("Times New Roman",15), bg = "#121212", fg="White", highlightthickness=0, borderwidth=0, state='disabled',  yscrollcommand=scrollbar3.set)
+scrollbar3.config(command=Output.yview)
 scrollbar3.pack(side=RIGHT, fill=Y)
-text_area.pack(side="left")
+Output.pack(side="left")
 
 lblLex=Label(root,text="Lexeme", font = ("Times New Roman",19), fg='white',bg='#171717')
 lblLex.place(x=775,y=100)
@@ -102,10 +99,6 @@ lblTok=Label(root,text="Token" , font = ("Times New Roman",19), fg='white',bg='#
 lblTok.place(x=1020,y=100)
 lblErr=Label(root,text="Errors", font = ("Times New Roman",19), fg='white',bg='#171717')
 lblErr.place(x=40,y=620)
-
-#OUTPUT TEXT AREA
-Output = Text(frame3, width = 50, height =28, font = ("Times New Roman",15), bg = "#121212", fg="White", highlightthickness=0, borderwidth=0, state='disabled',  yscrollcommand=scrollbar3.set)
-
 
 #Buttons or inside of Frame_Top
 Run_Button=Button(frame_top, width=175, height=35, image=photo_imageLexical, border=0,background='#0F0F0F', command = lambda:Take_input())
