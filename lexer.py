a0e60744177c8ef500faedc9f5fe67eaed97f8e5
Lance <47510561+PotentSama@users.noreply.github.com>
2022-01-07 17:25:25 +0800

    updated delims

--- lexer.py
+++ lexer.py
@@ -17,27 +17,28 @@ def run(lexeme):
         ('comment', r'(\/\*)[\s\S]*?(\*\/)'),
         ('lit_decposi', r'(([1-9]\d{0,8}\.\d{0,5})|(\d{0,9}\.\d{1}\d{0,4}))(?= |\n|\<\=|\>\=|\!\=|\<|\>|\=\=|\:|\,|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\)|\]|\}|$)'),
         ('lit_decnega', r'\-(([1-9]\d{0,8}\.\d{0,5})|(\d{0,9}\.\d{1}\d{0,4}))(?= |\n|\<\=|\>\=|\!\=|\<|\>|\=\=|\:|\,|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\)|\]|\}|$)'),
-        ('lit_intposi', r'([1-9]\d{0,8})(?= |\n|\<\=|\>\=|\!\=|\<|\>|\=\=|\:|\,|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\)|\]|\}|$)'),
-        ('lit_intnega', r'(\-[1-9]\d{0,8})(?= |\n|\<\=|\>\=|\!\=|\<|\>|\=\=|\:|\,|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\)|\]|\}|$)'),
-        ('lit_str', r'[\"\“]([^\"][ \S]*[^\"])?[\"\”](?= |\n|\:|\,|\)|\]|\}|$)'),
-        ('lit_bool', r'(True|False)(?= |\n|\:|\,|\)|\]|\}|$)'),
+        ('lit_intposi', r'([1-9]\d{0,8})(?=[\s\,\(\)\[\]\{\}]|\<\=|\>\=|\!\=|\<|\>|\=\=|\+|\-|(\/\/)|(\*\*)|\*|\/|\%)'),
+        ('lit_intnega', r'(\-[1-9]\d{0,8})(?=[\s\,\(\)\[\]\{\}]|\<\=|\>\=|\!\=|\<|\>|\=\=|\+|\-|(\/\/)|(\*\*)|\*|\/|\%)'),
+        ('lit_str', r'[\"\“]([^\"][ \S]*[^\"])?[\"\”](?=[\s\,\(\)\[\]\{\}])'),
+        ('lit_bool', r'(True|False)(?=[\s\,\(\)\[\]\{\}])'),
         ('relational', r'(\<\=|\>\=|\!\=|\<|\>|\=\=)(?= |[a-z]|[0-9]|\()'),
-        ('assignment', r'(\=|\-\=|\+\=|\*\=|\/\=|\*\*\=|\%\=|\/\/\=)(?= |[a-z]|[0-9]|\(|\")'),
+        ('equal', r'\=(?= |[a-z]|[0-9]|\(|\")'),
+        ('assignment', r'(\=|\-\=|\+\=|\*\=|\/\=|\*\*\=|\%\=|\/\/\=)(?= |[a-z]|[0-9]|\()'),
         ('arithmetic', r'(\+|\-|(\/\/)|(\*\*)|\*|\/|\%)(?= |[a-z]|[0-9]|\()'),
-        ('open_par', r'\((?= |[A-Za-z]|\)|\")'),
-        ('close_par', r'\)(?= |\n|\[|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\<\=|\>\=|\!\=|\<|\>|\=\=|And|Or|Not|\:|\))'),
-        ('open_brace',r'\{(?= |[a-z]|[0-9]|\})'),
-        ('close_brace',r'\}(?= |\n|And|Or|Not|\:|\)|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\<\=|\>\=|\!\=|\<|\>|\=\=)'),
-        ('open_bracket', r'\[(?=[ \nA-Z])'),
-        ('close_bracket', r'\](?=\n)'),
-        ('comma', r'\,(?= |[a-z])'),
-        ('colon', r'\:(?= |\n|\[)'),
-        ('period', r'\.(?=[a-z])'),
-        ('keyword', r'((Absorb|Boolean|Decimal|Discharge|Execute|Fixed|For|Generate|Integer|Return|Struct|String|Switch|Sys|Sys.Call|While)(?= ))|((Avoid|Break|Continue|End.Switch|Link.Start)(?=[ \n]))|((Default|Else)(?=\:))|((If|Elif|And|Or|Not)(?=[ \(]))|Link.End(?=\n|$)'),
+        ('open_par', r'\((?=[ANO0-9\(\)])'),
+        ('close_par', r'\)(?=And|Or|Not|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\<\=|\>\=|\!\=|\<|\>|\=\=|[\s\:\[\)])'),
+        ('open_brace',r'\{(?=[a-zA-Z\d\"])'),
+        ('close_brace',r'\}(?=[\s\)]|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\=|\-\=|\+\=|\*\=|\/\=|\*\*\=|\%\=|\/\/\=|\<\=|\>\=|\!\=|\<|\>|\=\=|And|Or|Not)'),
+        ('open_bracket', r'\[(?=[\sa-z\"\d])'),
+        ('close_bracket', r'\](?=\n|$)'),
+        ('comma', r'\,(?=[ A-Za-z])'),
+        ('colon', r'\:(?=[\s\[])'),
+        ('period', r'\.(?=[a-zA-Z\d])'),
+        ('keyword', r'((Integer|Decimal|String|Boolean|Struct|Generate|Absorb|Discharge|Switch|For|In|Sys|Sys.Call|Execute|Fixed|Return)(?= ))|((Default|Else)(?=\:))|((If|Elif|And|Or|Not|While)(?=[ \(]))|Link.End(?=\n|$)|(Link.Start|Link.End|End.Switch|Break|Continue|Avoid)(?=\n)'),
         ('non_keyword', r'(e(?i:nd\.switch)$|b(?i:reak)$|l(?i:ink\.start)$|l(?i:ink\.end)$|g(?i:enerate)$|s(?i:ys\.call)$|s(?i:ys)$|d(?i:ischarge)$|a(?i:bsorb)$|i(?i:f)$|e(?i:lif)$|e(?i:lse)$|s(?i:witch)$|e(?i:xecute)$|d(?i:efault)$|f(?i:or)$|w(?i:hile)$|e(?i:xit)$|c(?i:ontinue)$|a(?i:void)$|f(?i:ixed)$|s(?i:truct)$|v(?i:oid)$|r(?i:eturn)$|i(?i:nteger)$|b(?i:oolean)$|s(?i:tring)$|d(?i:ecimal)$|a(?i:nd)$|o(?i:r)$|n(?i:ot)$|t(?i:rue)$|f(?i:alse)$|i(?i:n)$)'),
         ('newline', r'\n'),
         ('tab_space', r'[ \t]'),
-        ('id', r'([a-z]\w{0,19})(?= |\n|\:|\,|\.|\(|\{|\[|\)|\}|\]|\<\=|\>\=|\!\=|\<|\>|\=\=|\=|\-\=|\+\=|\*\=|\/\=|\*\*\=|\%\=|\/\/\=|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|$)'),
+        ('id', r'([a-z]\w{0,19})(?=[\s\.\,\(\)\[\]\{\}]|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\=|\-\=|\+\=|\*\=|\/\=|\*\*\=|\%\=|\/\/\=|\<\=|\>\=|\!\=|\<|\>|\=\=)'),
         ('error', r'[^\s]+'),
     ]
     token_data = []
@@ -53,7 +54,7 @@ def run(lexeme):
             hasError = "Invalid character/delimiter"
             token_data.append(Token(kind,value,line_num,column,hasError))
             return token_data
-        if kind == 'relational' or kind == 'arithmetic' or kind == 'assignment' or kind == 'open_par' or kind == 'close_par' or kind == 'open_brace' or kind == 'close_brace' or kind == 'open_bracket' or kind == 'close_bracket' or kind == 'comma' or kind == 'colon' or kind == 'period' or kind == 'keyword':
+        if kind == 'equal' or kind == 'relational' or kind == 'arithmetic' or kind == 'assignment' or kind == 'open_par' or kind == 'close_par' or kind == 'open_brace' or kind == 'close_brace' or kind == 'open_bracket' or kind == 'close_bracket' or kind == 'comma' or kind == 'colon' or kind == 'period' or kind == 'keyword':
             kind = value
         elif kind == 'non_keyword':
             hasError = "Reserved word cannot be used as an identifier"
