21828914ea339cd2660c0b600d86796868eb1c1c
Lance <ronquillolance@gmail.com>
2022-06-11 11:56:30 +0800

    update

--- syntax_lexer.py
+++ syntax_lexer.py
@@ -1,9 +1,12 @@
 import ply.lex as lex
+from ply.lex import TOKEN
 
 
 # list of tokens
 tokens = [
     'comment',
+    'space',
+    
     #(?= )
     'Integer',
     'Decimal',
@@ -94,154 +97,165 @@ tokens = [
     'period',
     
     #identifier
-    'id'
+    'id',
+    
+    
+    
+    #errors
+    'error1', #non-keyword
+    'error2' #incomplete word
 ]
 
 def t_ignore_comment(t):
     r'(\/\*)[\s\S]*?(\*\/)'
 
-t_Integer = r'Integer(?= )'
-t_Decimal = r'Decimal(?= )'
-t_String = r'String(?= )'
-t_Boolean = r'Boolean(?= )'
-t_Struct = r'Struct(?= )'
-t_Generate = r'Generate(?= )'
-t_Absorb = r'Absorb(?= )'
-t_Discharge = r'Discharge(?= )'
-t_Switch = r'Switch(?= )'
-t_For = r'For(?= )'
-t_In = r'In(?= )'
-t_Sys = r'Sys(?= )'
-t_Sys_Call = r'Sys_Call(?= )'
-t_Execute = r'Execute(?= )'
-t_Fixed = r'Fixed(?= )'
-t_Return = r'Return(?= )'
+t_Integer = r'Integer'
+t_Decimal = r'Decimal'
+t_String = r'String'
+t_Boolean = r'Boolean'
+t_Struct = r'Struct'
+t_Generate = r'Generate'
+t_Absorb = r'Absorb'
+t_Discharge = r'Discharge'
+t_Switch = r'Switch'
+t_For = r'For'
+t_In = r'In'
+t_Sys = r'Sys'
+t_Sys_Call = r'Sys_Call'
+t_Execute = r'Execute'
+t_Fixed = r'Fixed'
+t_Return = r'Return'
 
 
-t_Default = r'Default(?=\:)'
-t_Else = r'Else(?=\:)'
+t_Default = r'Default'
+t_Else = r'Else'
 
 
-t_If = r'If(?=[ \(])'
-t_Elif = r'Elif(?=[ \(])'
-t_And = r'And(?=[ \(])'
-t_Or = r'Or(?=[ \(])'
-t_Not = r'Not(?=[ \(])'
-t_While = r'While(?=[ \(])'
+t_If = r'If'
+t_Elif = r'Elif'
+t_And = r'And'
+t_Or = r'Or'
+t_Not = r'Not'
+t_While = r'While'
 
 
-t_Link_End = r'Link\.End(?=\n|$)'
+t_Link_End = r'Link\.End'
 
 
-t_Link_Start = r'Link\.Start(?=\n)'
-t_End_Switch = r'End\.Switch(?=\n)'
-t_Break = r'Break(?=\n)'
-t_Continue = r'Continue(?=\n)'
-t_Avoid = r'Avoid(?=\n)'
+t_Link_Start = r'Link\.Start'
+t_End_Switch = r'End\.Switch'
+t_Break = r'Break'
+t_Continue = r'Continue'
+t_Avoid = r'Avoid'
 
 
 def t_lit_decposi(t):
-    r'(([0-9]\d{0,8}\.\d{0,5})|(\d{0,9}\.\d{1}\d{0,4}))(?= |\n|\<\=|\>\=|\!\=|\<|\>|\=\=|\:|\,|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\)|\]|\})'
+    r'(([0-9]\d{0,8}\.\d{0,5})|(\d{0,9}\.\d{1}\d{0,4}))'
     t.value = float(t.value)
     return t
 def t_lit_decnega(t):
-    r'\-(([0-9]\d{0,8}\.\d{0,5})|(\d{0,9}\.\d{1}\d{0,4}))(?= |\n|\<\=|\>\=|\!\=|\<|\>|\=\=|\:|\,|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\)|\]|\})'
+    r'\-(([0-9]\d{0,8}\.\d{0,5})|(\d{0,9}\.\d{1}\d{0,4}))'
     t.value = float(t.value)
     return t
 def t_lit_intposi(t):
-    r'([0-9]\d{0,8})(?=[\s\,\(\)\[\]\{\}]|\<\=|\>\=|\!\=|\<|\>|\=\=|\+|\-|(\/\/)|(\*\*)|\*|\/|\%)'
+    r'([0-9]\d{0,8})'
     t.value = int(t.value)
     return t
 def t_lit_intnega(t):
-    r'(\-[1-9]\d{0,8})(?=[\s\,\(\)\[\]\{\}]|\<\=|\>\=|\!\=|\<|\>|\=\=|\+|\-|(\/\/)|(\*\*)|\*|\/|\%)'
+    r'(\-[1-9]\d{0,8})'
     t.value = int(t.value)
     return t
-t_lit_str = r'[\"\“]{1}(([^\"^\n^\“^\”])*)?[\"\”]{1}(?=[\s\,\(\)\[\]\{\}\:])'
-t_lit_bool = r'(True|False)(?=[\s\,\(\)\[\]\{\}])'
+t_lit_str = r'[\"\“]{1}(([^\"^\n^\“^\”])*)?[\"\”]{1}'
+t_lit_bool = r'(True|False)'
 
 
 
 #relational
-t_less_than_equal = r'\<\=(?= |[a-z]|[0-9]|\()'
-t_great_than_equal = r'\>\=(?= |[a-z]|[0-9]|\()'
-t_not_equal = r'\!\=(?= |[a-z]|[0-9]|\()'
-t_less_than = r'\<(?= |[a-z]|[0-9]|\()'
-t_greater_than = r'\>(?= |[a-z]|[0-9]|\()'
-t_equal_equal = r'\=\=(?= |[a-z]|[0-9]|\()'
+t_less_than_equal = r'\<\='
+t_great_than_equal = r'\>\='
+t_not_equal = r'\!\='
+t_less_than = r'\<'
+t_greater_than = r'\>'
+t_equal_equal = r'\=\='
 
 
 #assignment
 def t_minus_equal(t):
-    r'\-\=(?= |[a-z]|[0-9]|\()'
+    r'\-\='
     return t
 def t_plus_equal(t):
-    r'\+\=(?= |[a-z]|[0-9]|\()'
+    r'\+\='
     return t
 def t_times_equal(t):
-    r'\*\=(?= |[a-z]|[0-9]|\()'
+    r'\*\='
     return t
 def t_divide_equal(t):
-    r'\/\=(?= |[a-z]|[0-9]|\()'
+    r'\/\='
     return t
 def t_times_times_equal(t):
-    r'\*\*\=(?= |[a-z]|[0-9]|\()'
+    r'\*\*\='
     return t
 def t_modulo_equal(t):
-    r'\%\=(?= |[a-z]|[0-9]|\()'
+    r'\%\='
     return t
 def t_divide_divide_equal(t):
-    r'\/\/\=(?= |[a-z]|[0-9]|\()'
+    r'\/\/\='
     return t
-def t_equal(t):
-    r'\=(?=[ ]|[a-z]|[0-9]|\(|\"|\“)'
-    return t
+t_equal = r'\='
 
 #arithmetic
 def t_plus(t):
-    r'\+(?= |[a-z]|[0-9]|\()'
+    r'\+'
     return t
 def t_minus(t):
-    r'\-(?= |[a-z]|[0-9]|\()'
+    r'\-'
     return t
 def t_times(t):
-    r'\*(?= |[a-z]|[0-9]|\()' 
+    r'\*' 
     return t
 def t_times_times(t):
-    r'\*\*(?= |[a-z]|[0-9]|\()'
+    r'\*\*'
     return t
 def t_divide(t):
-    r'\/(?= |[a-z]|[0-9]|\()'
+    r'\/'
     return t
 def t_divide_divide(t):
-    r'\/\/(?= |[a-z]|[0-9]|\()'
+    r'\/\/'
     return t
 def t_modulo(t):
-    r'\%(?= |[a-z]|[0-9]|\()'
+    r'\%'
     return t
 
 
-t_open_par = r'\((?=[a-zANOISDB0-9\(\)])'
-t_close_par = r'\)(?=And|Or|Not|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\<\=|\>\=|\!\=|\<|\>|\=\=|[\s\:\[\)])'
-t_open_brace = r'\{(?=[a-zA-Z\d\"|\“|\}])'
-t_close_brace = r'\}(?=[\s\)]|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\=|\-\=|\+\=|\*\=|\/\=|\*\*\=|\%\=|\/\/\=|\<\=|\>\=|\!\=|\<|\>|\=\=|And|Or|Not)'
-t_open_bracket = r'\[(?=[\sa-z\"\“\d])'
-t_close_bracket =  r'\](?= |\n|$)'
+t_open_par = r'\('
+t_close_par = r'\)'
+t_open_brace = r'\{'
+t_close_brace = r'\}'
+t_open_bracket = r'\['
+t_close_bracket =  r'\]'
 
 
-t_comma = r'\,(?=[ A-Za-z0-9])'
-t_colon = r'\:(?=[\s\[])'
-t_period =  r'\.(?=[a-zA-Z\d])'
+t_comma = r'\,'
+t_colon = r'\:'
+t_period =  r'\.'
 
-t_id = r'([a-z]\w{0,19})(?=[\s\.\,\(\)\[\]]|\+|\-|(\/\/)|(\*\*)|\*|\/|\%|\=|\-\=|\+\=|\*\=|\/\=|\*\*\=|\%\=|\/\/\=|\<\=|\>\=|\!\=|\<|\>|\=\=|\:|\{)'
+t_id = r'([a-z]\w{0,19})'
+
+t_error1 = r'(e(?i:nd\.switch)|b(?i:reak)|l(?i:ink\.start)|l(?i:ink\.end)|g(?i:enerate)|s(?i:ys\.call)|s(?i:ys)|d(?i:ischarge)|a(?i:bsorb)|i(?i:f)|e(?i:lif)|e(?i:lse)|s(?i:witch)|e(?i:xecute)|d(?i:efault)|f(?i:or)|w(?i:hile)|e(?i:xit)|c(?i:ontinue)|a(?i:void)|f(?i:ixed)|s(?i:truct)|v(?i:oid)|r(?i:eturn)|i(?i:nteger)|b(?i:oolean)|s(?i:tring)|d(?i:ecimal)|a(?i:nd)|o(?i:r)|n(?i:ot)|t(?i:rue)|f(?i:alse)|i(?i:n))'
+t_error2 = r'[\S]+'
+
+
+def t_ignore_space(t):
+    r'[ \t]'
 
 # Define a rule so we can track line numbers
 def t_newline(t):
-    r'[ \n\t]'
-    if(t.value == '\n'):
-        t.lexer.lineno += len(t.value)
+    r'\n'
+    t.lexer.lineno += len(t.value)
 
 
 def t_error(t):
      t.lexer.skip(1)
+     return t
 
 lexer_syntax = lex.lex()
